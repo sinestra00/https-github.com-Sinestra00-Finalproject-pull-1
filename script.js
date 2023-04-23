4dfb7866812c610d7699a942fc16f303d5e0b259
sinestra00 <sharri16@terpmail.umd.edu>
2023-04-23 14:37:05 -0400

    API link and CSS font color

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 let timer
 let deleteFirstPhotoDelay
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://dog-breeds2.p.rapidapi.com/dog_breeds",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-		"X-RapidAPI-Host": "dog-breeds2.p.rapidapi.com"
-	}
-};
+const settings = { 
+  "async": true, "crossDomain": true, 
+  "url": "https://dog-breeds2.p.rapidapi.com/dog_breeds", 
+  "method": "GET", 
+  "headers": { 
+  "X-RapidAPI-Key": "c379b54daamsh6c156222478144bp10ee89jsn39eb9e4f0847", 
+  "X-RapidAPI-Host": "dog-breeds2.p.rapidapi.com" } }; $.ajax(settings).done(function (response) { console.log(response); });
 
 $.ajax(settings).done(function (response) {
 	console.log(response);
@@ -31,13 +28,15 @@ function createBreedList(breedList) {
 
 async function loadByBreed(breed) {
   if (breed != "Choose a dog breed") {
-    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+    const response = await fetch("https://dog-breeds2.p.rapidapi.com/dog_breeds")
     const data = await response.json()
     console.log(data.message[0])
        document.getElementById("slideshow").innerHTML =
          `<img src="${data.message[0]}" alt="Italian Trulli">`
   }
 }
+
+
 
 
 
